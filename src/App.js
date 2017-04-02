794846aadc2cf4de62642db3be26319e76b15fae
Paul Shen <paulshen0@gmail.com>
2017-04-01 23:26:21 -0700

    connect to graphql

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import { ApolloProvider } from 'react-apollo';
+
+import { GraphqlClient, Store } from './data';
 
 class App extends Component {
   render() {
     return (
-      <div />
+      <ApolloProvider store={Store} client={GraphqlClient}>
+        <div />
+      </ApolloProvider>
     );
   }
 }
---
+++ src/data/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+
+const networkInterface = createNetworkInterface('https://api.graph.cool/simple/v1/cj0y20quf2p7t0178ok9s4wp2');
+
+export const GraphqlClient = new ApolloClient({
+  networkInterface,
+});
+
+export const Store = createStore(
+  combineReducers({
+    apollo: GraphqlClient.reducer(),
+  }),
+  // initial state
+  {},
+  compose(
+    applyMiddleware(GraphqlClient.middleware()),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )
+);
